71cf385107bf893a98063acd5da68625a7e29fdd
João Vitor <joaovitor55670@gmail.com>
2024-04-18 14:26:10 -0300

    Declarando variáveis iniciais e chamando os Hooks useState e useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [title, setTitle] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showDiv, setShowDiv] = useState(false);
+
   return (
     <div className="App">
 
